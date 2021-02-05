86f70698edfb1b5f2423cdf2562973b629c951e6
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-05 08:12:20 +0000

    remaining components - excluding modal, live + time

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-  import { onMount, setContext } from "svelte";
+  import {onMount, setContext} from "svelte";
   import Router from "svelte-spa-router";
   import Sidebar from "./components/navigators/Sidebar.svelte";
   import Blank from "./pages/support/Blank.svelte";
@@ -13,22 +13,47 @@
   import NotFound from "./pages/support/NotFound.svelte";
   import MainNavigator from "./components/navigators/MainNavigator.svelte";
   import Logout from "./pages/support/Logout.svelte";
-  import { Cache } from "./services/course/cache";
-  import { handleAuthentication } from "./services/analytics/auth-service";
-  import { AnalyticsService } from "./services/analytics/analytics-service";
+  import {Cache} from "./services/course/cache";
+  import {handleAuthentication} from "./services/analytics/auth-service";
+  import {AnalyticsService} from "./services/analytics/analytics-service";
+  import Search from "./pages/Search.svelte";
 
+  setContext("cache", new Cache());
+  const analytics = new AnalyticsService();
+  setContext("analytics", analytics);
 
-  let count: number = 0;
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000);
-    return () => {
-      clearInterval(interval);
-    };
+  onMount(async () => {
+    const path = document.location.href;
+    if (path.includes("access_token")) {
+      const token = path.substring(path.indexOf("#") + 1);
+      handleAuthentication(token, analytics);
+    }
   });
+
+  let routes = {
+    "/": Blank,
+    "/course/*": Course,
+    "/topic/*": Topic,
+    "/talk/*": Talk,
+    "/video/*": Video,
+    "/lab/*": Lab,
+    "/wall/*": Wall,
+    "/authorize/": Blank,
+    "/logout": Logout,
+    "/search/*": Search,
+    "/all/": AllCourses,
+    "*": NotFound
+  };
+
+  if (localStorage.theme && localStorage.theme === "dark") {
+    window.document.body.classList.toggle("dark");
+  }
 </script>
 
-<div class="App">
-<h1 class="p-4 border"> test </h1>
+<div class="antialiased bg-gray-50 text-gray-900 font-sans dark:bg-black dark:text-gray-100 min-h-screen">
+  <Sidebar/>
+  <MainNavigator/>
+  <Router {routes}/>
 </div>
 
 <style global>
