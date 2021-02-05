6a0e4ea2c878049e0a8e9ea58ab6809512384604
Eamonn de Leastar <edeleastar@wit.ie>
2021-02-05 07:35:18 +0000

    add tailwind typography + clamp plugins + enable dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,52 @@
 module.exports = {
-  purge: [
-    "./src/**/*.svelte",
-  ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.svelte"],
+  },
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#24283b",
+      },
+
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            a: {
+              color: theme("colors.blue.700"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+                textDecoration: "none",
+              },
+            },
+          },
+        },
+
+        dark: {
+          css: {
+            color: "white",
+            h1: { color: "white" },
+            h2: { color: "white" },
+            h3: { color: "white" },
+            code: { color: "white" },
+            strong: { color: "white" },
+            a: {
+              color: theme("colors.gray.100"),
+              "&:hover": {
+                color: "#9ECE6A",
+              },
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
-    extend: {},
+    typography: ["dark"],
   },
-  plugins: []
-}
+
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")],
+};
